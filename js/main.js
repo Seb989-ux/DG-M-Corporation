6fce968fc586c6af9e3d81efd18d7468e376bdc4
Sebus989 <wisniewskisebastian989@gmail.com>
2023-04-14 19:32:43 +0200

    added navBtn is-active function

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ const navMobile = document.querySelector('.nav.mobile')
 const navBtn = document.querySelector('.hamburger')
 const footerYear = document.querySelector('.footer__year')
 
+const handleNav = () => {
+	navBtn.classList.toggle('is-active')
+}
+navBtn.addEventListener('click', handleNav)
+
+
+
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
 	footerYear.innerText = year
